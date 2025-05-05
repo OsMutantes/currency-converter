8dd026a9333dd3c24f58318041c8e9cfeb07bb7c
italomelo <italord.melo@gmail.com>
2025-05-04 22:54:41 -0300

    created footer, changed language, added favicon

--- composeApp/src/commonMain/kotlin/com/github/mutantes/components/Body.kt
+++ composeApp/src/commonMain/kotlin/com/github/mutantes/components/Body.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,27 +19,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
-import com.github.mutantes.Currency
 import com.github.mutantes.model.MainViewModel
 import com.github.mutantes.style.Colors
 import currency_converter.composeapp.generated.resources.Res
 import currency_converter.composeapp.generated.resources.inter_regular
 import currency_converter.composeapp.generated.resources.inter_semibold
 import org.jetbrains.compose.resources.Font
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview
 fun Body() {
 
     val homeViewModel = remember { MainViewModel() }
     val state = homeViewModel.screenState.collectAsState().value
 
-    LaunchedEffect(Unit) { homeViewModel.getRates(state.firstCurrency,1.0) }
+    LaunchedEffect(Unit) { homeViewModel.getRates(state.firstCurrency, 1.0) }
 
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -68,14 +65,14 @@ fun Body() {
                 verticalArrangement = Arrangement.Top,
             ) {
                 Text(
-                    "Conversor de moedas", color = Colors.gray100, style = TextStyle(
+                    "Currency converter", color = Colors.gray100, style = TextStyle(
                         fontFamily = FontFamily(Font(Res.font.inter_semibold)),
                         fontSize = 16.sp
                     )
                 )
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    "Digite o valor e escolha as moedas de conversão",
+                    "Enter the amount and choose the conversion currencies",
                     color = Colors.gray200,
                     style = TextStyle(
                         fontFamily = FontFamily(Font(Res.font.inter_regular)),
--- composeApp/src/commonMain/kotlin/com/github/mutantes/screens/Home.kt
+++ composeApp/src/commonMain/kotlin/com/github/mutantes/screens/Home.kt
@@ -8,17 +8,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.github.mutantes.components.Body
+import com.github.mutantes.components.Footer
 import com.github.mutantes.components.Header
 import com.github.mutantes.style.Colors
 
 @Composable
 fun Home() {
-    Column(modifier = Modifier
-        //.background(Colors.gray100) // cor pra debug
-        .background(Colors.gray400)
-        .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(118.dp)) {
+    Column(
+        modifier = Modifier
+            //.background(Colors.gray100) // cor pra debug
+            .background(Colors.gray400)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(118.dp)
+    ) {
         Header()
         Body()
+        Footer()
     }
 }
